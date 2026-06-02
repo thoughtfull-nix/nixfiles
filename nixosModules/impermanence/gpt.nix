2b4cb56d779d144bd4c111805a90f6bbbf62efaf
technosophist <technosophist@thoughtfull.systems>
2026-06-02 11:15:13 +0000

    Fix configuration nesting

--- nixosModules/impermanence/gpt.nix
+++ nixosModules/impermanence/gpt.nix
@@ -14,69 +14,67 @@ let
     root
     snapshots
     swap
-  ;
+    ;
 in
 {
-  config = {
-    assertions = [
-      {
-        assertion = !enable || encrypted.device != null;
-        message = "thoughtfull.impermanence.disko.encrypted.device is not configured";
-      }
-    ];
-    disko.devices.disk.main = mkIf enable {
-      content = {
-        partitions = {
-          ESP = {
-            content = {
-              format = "vfat";
-              mountOptions = boot.mountOptions;
-              mountpoint = "/boot";
-              type = "filesystem";
-            };
-            size = boot.size;
-            type = "EF00";
+  assertions = [
+    {
+      assertion = !enable || encrypted.device != null;
+      message = "thoughtfull.impermanence.disko.encrypted.device is not configured";
+    }
+  ];
+  disko.devices.disk.main = mkIf enable {
+    content = {
+      partitions = {
+        ESP = {
+          content = {
+            format = "vfat";
+            mountOptions = boot.mountOptions;
+            mountpoint = "/boot";
+            type = "filesystem";
           };
-          luks = {
-            content = {
-              content = {
-                extraArgs = [ "-f" ];
-                subvolumes = {
-                  "/nix" = {
-                    mountOptions = nix.mountOptions;
-                    mountpoint = "/nix";
-                  };
-                  "/${persistentName}" = {
-                    mountOptions = persistent.mountOptions;
-                    mountpoint = persistent.mountPoint;
-                  };
-                  "/${root.name}" = {
-                    mountOptions = root.mountOptions;
-                    mountpoint = "/";
-                  };
-                  "/${snapshotsName}" = {
-                    mountOptions = snapshots.mountOptions;
-                    mountpoint = snapshots.mountPoint;
-                  };
-                  "/swap" = {
-                    mountpoint = "/.swapvol";
-                    swap.swapfile.size = swap.size;
-                  };
-                };
-                type = "btrfs";
-              };
-              name = encryptedName;
-              passwordFile = "/tmp/luks-recovery-passphrase.txt";
-              settings.allowDiscards = true;
-              type = "luks";
-            };
-            size = "100%";
-          };
+          size = boot.size;
+          type = "EF00";
         };
-        type = "gpt";
+        luks = {
+          content = {
+            content = {
+              extraArgs = [ "-f" ];
+              subvolumes = {
+                "/nix" = {
+                  mountOptions = nix.mountOptions;
+                  mountpoint = "/nix";
+                };
+                "/${persistentName}" = {
+                  mountOptions = persistent.mountOptions;
+                  mountpoint = persistent.mountPoint;
+                };
+                "/${root.name}" = {
+                  mountOptions = root.mountOptions;
+                  mountpoint = "/";
+                };
+                "/${snapshotsName}" = {
+                  mountOptions = snapshots.mountOptions;
+                  mountpoint = snapshots.mountPoint;
+                };
+                "/swap" = {
+                  mountpoint = "/.swapvol";
+                  swap.swapfile.size = swap.size;
+                };
+              };
+              type = "btrfs";
+            };
+            name = encryptedName;
+            passwordFile = "/tmp/luks-recovery-passphrase.txt";
+            settings.allowDiscards = true;
+            type = "luks";
+          };
+          size = "100%";
+        };
       };
-      device = encrypted.device;
-      type = "disk";
+      type = "gpt";
     };
+    device = encrypted.device;
+    type = "disk";
   };
 }
