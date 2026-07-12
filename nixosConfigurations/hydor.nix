1e5f7185342e734d206a473b8606071c7a7c916b
technosophist <technosophist@thoughtfull.systems>
2026-07-11 20:28:18 -0400

    Simplify indentation

--- nixosConfigurations/hydor.nix
+++ nixosConfigurations/hydor.nix
@@ -9,17 +9,15 @@
           ./hydor/hardware-configuration.nix
         ];
         networking.hostName = "hydor";
-        services = {
-          syncthing = {
-            settings.folders = {
-              archive.enable = true;
-              obsidian.enable = true;
-              obsidian-work.enable = true;
-              org.enable = true;
-              org-work.enable = true;
-            };
-            thoughtfull.passwordFile = ./hydor/secrets/syncthing-passphrase.age;
+        services.syncthing = {
+          settings.folders = {
+            archive.enable = true;
+            obsidian.enable = true;
+            obsidian-work.enable = true;
+            org.enable = true;
+            org-work.enable = true;
           };
+          thoughtfull.passwordFile = ./hydor/secrets/syncthing-passphrase.age;
         };
         system.stateVersion = "25.11";
         thoughtfull = {
